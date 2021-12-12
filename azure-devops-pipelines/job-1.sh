2189ee521e66e2b716cb27b626a210bc735a847b
M. Rafli Al Khadafi <raflyalk@gmail.com>
2021-12-12 15:13:44 +0700

    Update job-1.sh

--- azure-devops-pipelines/job-1.sh
+++ azure-devops-pipelines/job-1.sh
@@ -1,4 +1,4 @@
 echo $servicePrincipalId
 echo $tenantId
-accessToken=$(curl -X POST -H 'Content-Type: application/x-www-form-urlencoded' -d 'grant_type=client_credentials&client_id=${servicePrincipalId}&resource=2ff814a6-3304-4ab8-85cb-cd0e6f879c1d&client_secret=${servicePrincipalKey}' https://login.microsoftonline.com/${tenantId}/oauth2/token)
+accessToken=$(curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials&client_id=${servicePrincipalId}&resource=2ff814a6-3304-4ab8-85cb-cd0e6f879c1d&client_secret=${servicePrincipalKey}" https://login.microsoftonline.com/${tenantId}/oauth2/token)
 echo $accessToken
